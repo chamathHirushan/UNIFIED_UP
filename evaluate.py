e86d4e2434316c2f693a7254d68af8824919e71f
chamathHirushan <chamathg.21@cse.mrt.ac.lk>
2025-10-15 10:00:28 +0530

    fix

--- evaluate.py
+++ evaluate.py
@@ -63,7 +63,7 @@ class UnifiedQAWithRetrieval:
         inputs = {k: v.to(device) for k, v in inputs.items()}
 
         output_ids = self.model.generate(**inputs, **generate_kwargs)
-        return self.tokenizer.decode(output_ids[0], skip_special_tokens=True)
+        return self.tokenizer.decode(output_ids[0], skip_special_tokens=True, clean_up_tokenization_spaces=True)
 
 
 # Load the QA system
